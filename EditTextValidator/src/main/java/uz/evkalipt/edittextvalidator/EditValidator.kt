4528021817beaf473c612240222804d376745c0d
AkmalValiev <akki77777@mail.ru>
2022-04-30 22:26:55 +0500

    edit library

--- EditTextValidator/src/main/java/uz/evkalipt/edittextvalidator/EditValidator.kt
+++ EditTextValidator/src/main/java/uz/evkalipt/edittextvalidator/EditValidator.kt
@@ -7,7 +7,6 @@ import androidx.core.widget.addTextChangedListener
 object EditValidator {
 
     fun passwordValidator(password:EditText, rePassword:EditText):Boolean{
-        var boolean = false
         rePassword.addTextChangedListener {
             if (password.text.toString()
                     .trim() != "" && password.text.toString() == rePassword.text.toString()
@@ -18,7 +17,6 @@ object EditValidator {
                     R.drawable.ic_baseline_check_24,
                     0
                 )
-                boolean = true
             } else {
                 rePassword.setCompoundDrawablesWithIntrinsicBounds(
                     0,
@@ -26,7 +24,6 @@ object EditValidator {
                     R.drawable.ic_baseline_info_24,
                     0
                 )
-                boolean = false
             }
         }
 
@@ -41,7 +38,6 @@ object EditValidator {
                         R.drawable.ic_baseline_check_24,
                         0
                     )
-                    boolean = true
                 }
             } else {
                 if (rePassword.text.toString().trim()!="") {
@@ -51,16 +47,14 @@ object EditValidator {
                         R.drawable.ic_baseline_info_24,
                         0
                     )
-                    boolean = false
                 }
             }
         }
 
-        return boolean
+        return password.text.toString()==rePassword.text.toString()
     }
 
     fun emailAddress(emailAddress:EditText):Boolean{
-        var emailAddress1 = false
         emailAddress.addTextChangedListener {
             if (emailAddress.text.toString().trim() != "" && emailAddress.text.toString()
                     .substring(0, 1) != "@" && emailAddress.text.toString().contains("@")
@@ -71,7 +65,6 @@ object EditValidator {
                     R.drawable.ic_baseline_check_24,
                     0
                 )
-                emailAddress1 = true
             } else {
                 emailAddress.setCompoundDrawablesWithIntrinsicBounds(
                     0,
@@ -79,15 +72,13 @@ object EditValidator {
                     R.drawable.ic_baseline_info_24,
                     0
                 )
-                emailAddress1 = false
             }
         }
-        return emailAddress1
+        return emailAddress.text.toString().trim() != "" && emailAddress.text.toString()
+            .substring(0, 1) != "@" && emailAddress.text.toString().contains("@")
     }
 
     fun ipAddress(iPAddress: EditText):Boolean{
-
-        var iPAddress1 = false
 
         iPAddress.addTextChangedListener {
             try {
@@ -137,22 +128,68 @@ object EditValidator {
 
                 if (ip1 in 0..255 && ip2 in 0..255 && ip3 in 0..255 && ip4 in 0..255){
                     iPAddress.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_check_24, 0)
-                    iPAddress1 = true
                 }else{
                     iPAddress.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_info_24, 0)
-                    iPAddress1 = false
                 }
             }catch (e:NumberFormatException){
                 iPAddress.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_info_24, 0)
-                iPAddress1 = false
             }
 
         }
-        return iPAddress1
+
+        try {
+            var ip1 = -1
+            var ip2 = -1
+            var ip3 = -1
+            var ip4 = -1
+
+            var count = 0
+
+            var str = iPAddress.text.toString()
+            var length = iPAddress.text.toString().length
+            var index = -1
+            for (i in 0 until length){
+                if (str[i].toString() == "."){
+                    ip1 = str.substring(0, i).toInt()
+                    count++
+                    index = i
+                    break
+                }
+            }
+
+            for (i in index+1 until length){
+                if (str[i].toString() == "."){
+                    ip2 = str.substring(index+1, i).toInt()
+                    index = i
+                    count++
+                    break
+                }
+            }
+
+            for (i in index+1 until length){
+                if (str[i].toString() == "."){
+                    ip3 = str.substring(index+1, i).toInt()
+                    index = i
+                    count++
+                    break
+                }
+            }
+
+            for (i in index+1 until length) {
+                if (count==3) {
+                    ip4 = str.substring(index + 1, length).toInt()
+                }
+            }
+
+
+            return ip1 in 0..255 && ip2 in 0..255 && ip3 in 0..255 && ip4 in 0..255
+        }catch (e:NumberFormatException){
+            return false
+        }
+
     }
 
     fun telephoneNumber(telephoneNumber: EditText):Boolean{
-        var telephoneNumber1 = false
         telephoneNumber.addTextChangedListener {
             if (telephoneNumber.text.toString().trim() != "") {
                 if (telephoneNumber.text.toString().substring(
@@ -167,7 +204,6 @@ object EditValidator {
                         R.drawable.ic_baseline_check_24,
                         0
                     )
-                    telephoneNumber1 = true
                 } else {
                     telephoneNumber.setCompoundDrawablesWithIntrinsicBounds(
                         0,
@@ -175,40 +211,38 @@ object EditValidator {
                         R.drawable.ic_baseline_info_24,
                         0
                     )
-                    telephoneNumber1 = false
                 }
             }
         }
-        return telephoneNumber1
+        return telephoneNumber.text.toString().substring(
+            0,
+            1
+        ) == "+" && telephoneNumber.text.toString().length >= 10 && telephoneNumber.text.toString().length <= 15 && telephoneNumber.text.toString()
+            .trim() != ""
     }
 
     fun zipCode(zipCode: EditText):Boolean{
-        var zipCode1 = false
         zipCode.addTextChangedListener {
             if (zipCode.text.toString().trim()!="" && zipCode.text.toString().length<=10 && zipCode.text.toString().length>=5){
                 zipCode.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_check_24,0)
-                zipCode1 = true
             }else{
                 zipCode.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_info_24, 0)
-                zipCode1 = false
             }
         }
-        return zipCode1
+        return zipCode.text.toString().trim()!="" && zipCode.text.toString().length<=10 && zipCode.text.toString().length>=5
+
     }
 
     fun year(year: EditText):Boolean{
-        var year1 = false
 
         year.addTextChangedListener {
             if (year.text.toString().trim()!="" && year.text.toString().toInt()<=2004 && year.text.toString().toInt()>=1910){
                 year.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_check_24, 0)
-                year1 = true
             }else{
                 year.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_info_24, 0)
-                year1 = false
             }
         }
-        return year1
+        return year.text.toString().trim()!="" && year.text.toString().toInt()<=2004 && year.text.toString().toInt()>=1910
     }
 
 }
